d812badfdfb6137762a87a34e778c7b2b0b85dbb
Chellys Castillo <chellys.castillo@gmail.com>
2023-07-20 16:30:25 -0400

    done about responsive page

---
+++ src/components/Education.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Education.css';
+import certifications from '../constants/certifications';
+const Education = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+  };
+  return (
+    <div>
+      <Slider {...settings}>
+        {certifications.map(({ src, alt }, index) => {
+          return (
+            <div key={index}>
+              <img src={src} alt={alt} className="style__certifications" />
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
+};
+
+export default Education;
+
---
+++ src/constants/certifications.jsx
@@ -0,0 +1,31 @@
+import responsiveWebDesign from '../assets/certifications/responsiveWebDesign.png';
+import javascript from '../assets/certifications/javascript.png';
+import frontEnd from '../assets/certifications/frontEnd.png';
+import bilingual from '../assets/certifications/bilingual.jpg';
+
+const certifications = [
+  {
+    id: 1,
+    src: frontEnd,
+    alt: 'frontEnd',
+  },
+  {
+    id: 2,
+    src: javascript,
+    alt: 'javascript',
+  },
+  {
+    id: 3,
+    src: responsiveWebDesign,
+    alt: 'responsive web design',
+  },
+
+  {
+    id: 4,
+    src: bilingual,
+    alt: 'bilingual',
+  },
+];
+
+export default certifications;
+
