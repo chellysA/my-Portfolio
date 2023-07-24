575201bb62b1c8a2560386491f5fc5c05612bfab
Chellys Castillo <chellys.castillo@gmail.com>
2023-07-24 12:41:14 -0400

    fix: navbar desk

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -9,30 +9,30 @@ const NavBar = () => {
     <nav className="nav">
       <div className={`${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`}>
         <ul>
-          <li className="nav__item">
-            <FaHome className="nav--icon" />
-            <NavLink exact to="/" className="nav__link" onClick={() => setShowMenu(!showMenu)}>
+          <NavLink exact to="/" onClick={() => setShowMenu(!showMenu)}>
+            <li className="nav__item">
+              <FaHome className="nav--icon" />
               <h3 className="nav__name">Home</h3>
-            </NavLink>
-          </li>
-          <li className="nav__item">
-            <FaUser className="nav--icon" />
-            <NavLink to="/about" className="nav__link" onClick={() => setShowMenu(!showMenu)}>
+            </li>
+          </NavLink>
+          <NavLink to="/about" className="nav__link" onClick={() => setShowMenu(!showMenu)}>
+            <li className="nav__item">
+              <FaUser className="nav--icon" />
               <h3 className="nav__name">About</h3>
-            </NavLink>
-          </li>
-          <li className="nav__item">
-            <FaFolderOpen className="nav--icon" />
-            <NavLink to="/portfolio" className="nav__link" onClick={() => setShowMenu(!showMenu)}>
+            </li>
+          </NavLink>
+          <NavLink to="/portfolio" className="nav__link" onClick={() => setShowMenu(!showMenu)}>
+            <li className="nav__item">
+              <FaFolderOpen className="nav--icon" />
               <h3 className="nav__name">Portfolio</h3>
-            </NavLink>
-          </li>
-          {/*<li className="nav__item">
+            </li>
+          </NavLink>
+          {/*  <NavLink to="/contact" className="nav__link" onClick={() => setShowMenu(!showMenu)}>
+          <li className="nav__item">
             <FaEnvelopeOpen className="nav--icon" />
-            <NavLink to="/contact" className="nav__link" onClick={() => setShowMenu(!showMenu)}>
               <h3 className="nav__name">Contact</h3>
-            </NavLink>
-  </li>*/}
+  </li>
+  </NavLink>*/}
         </ul>
       </div>
 
