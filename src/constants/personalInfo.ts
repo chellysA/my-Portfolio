eb44c3103f2b46e4d925f37f3d3c2ae2cfdba434
chellysC <chellys_castillo@readychatai.com>
2024-12-30 14:17:56 -0400

    feat:

--- src/constants/personalInfo.ts
+++ src/constants/personalInfo.ts
@@ -5,16 +5,14 @@ interface PersonalInfoProps {
 }
 
 export const personalInfo1: PersonalInfoProps[] = [
-  { id: 1, title: 'First Name :', description: ' Chellys.' },
-  { id: 2, title: 'Last Name :', description: ' Castillo.' },
-  { id: 3, title: 'Age :', description: ' 26 Years Old.' },
-  { id: 4, title: 'Nationality :', description: ' Venezuelan.' },
+  { id: 1, title: 'First Name :', description: ' Chellys' },
+  { id: 2, title: 'Last Name :', description: ' Castillo' },
+  { id: 4, title: 'Nationality :', description: ' Venezuelan' },
 ];
 
 export const personalInfo2: PersonalInfoProps[] = [
-  { id: 6, title: 'Address :', description: ' Caracas-Venezuela.' },
-  { id: 7, title: 'Phone :', description: ' +58424-275-4685' },
+  { id: 6, title: 'Address :', description: ' Miranda-Venezuela.' },
+  { id: 7, title: 'Phone :', description: ' +58-424-275-4685' },
   { id: 8, title: 'Email :', description: ' chellys.castillo@gmail.com' },
   { id: 9, title: 'Languages :', description: ' Spanish, English.' },
 ];
-
